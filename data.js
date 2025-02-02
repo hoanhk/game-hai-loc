bb694ee577b0a4456cf7ad73ee74f4094e26af7c
hoanhk <hoanhk@sendo.vn>
2025-02-02 16:47:21 +0700

    Thêm trang tổng hợp kết quả

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
-const quotes = [
-    "Học thầy không tày học bạn.",
-    "Có công mài sắt, có ngày nên kim.",
-    "Đoàn kết là sức mạnh.",
-    "Không có gì quý hơn độc lập, tự do.",
-    "Chăm chỉ là chìa khóa của thành công.",
-    "Sống là không chờ đợi.",
-    "Hãy theo đuổi đam mê, thành công sẽ theo đuổi bạn.",
-    "Đường dài mới biết ngựa hay.",
-    "Hãy làm những gì bạn yêu thích, và bạn sẽ không phải làm việc một ngày nào trong đời.",
-    "Không có áp lực, không có kim cương.",
-  ];
+export const quotes = [
+  "Học thầy không tày học bạn.",
+  "Có công mài sắt, có ngày nên kim.",
+  "Đoàn kết là sức mạnh.",
+  "Không có gì quý hơn độc lập, tự do.",
+  "Chăm chỉ là chìa khóa của thành công.",
+  "Sống là không chờ đợi.",
+  "Hãy theo đuổi đam mê, thành công sẽ theo đuổi bạn.",
+  "Đường dài mới biết ngựa hay.",
+  "Hãy làm những gì bạn yêu thích, và bạn sẽ không phải làm việc một ngày nào trong đời.",
+  "Không có áp lực, không có kim cương."
+];
